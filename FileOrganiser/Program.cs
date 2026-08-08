2e77af548a58f4fd6246570ee3cf3c6ff79f73df
Kxllve <coreyprescott416@protonmail.com>
2026-08-08 13:57:34 +0100

    Added file counting and better file sorting

--- FileOrganiser/Program.cs
+++ FileOrganiser/Program.cs
@@ -27,52 +27,66 @@ namespace FileOrganiser
         public static void SortFiles(string userDefinedPath)
         {
             string[] folders = { "Documents", "Images", "Music", "Videos", "Compressed" };
+            int totalFilesMoved = 0;
+            
+            // File format arrays
+            string[] fileFormatsDocuments = { ".txt", ".pdf", ".docx", ".psd", ".kra", ".aep", ".csp" };
+            string[] fileFormatsImages = { ".png", ".jpg", ".jpeg", ".webp", ".gif", ".svg" };
+            string[] fileFormatsMusic = { ".mp3", ".wav", ".aac" };
+            string[] fileFormatsVideos = { ".mp4", ".mov", ".mkv", ".avi", ".webm" };
+            string[] fileFormatsCompressed = { ".zip", ".rar", ".7z", ".tar", ".tar.gz" };
+
             for (int i = 0; i < folders.Length; i++)
             {
                 string currentFolder = $"{userDefinedPath}/{folders[i]}";
                 Console.WriteLine($"Creating {currentFolder}...");
                 Directory.CreateDirectory(currentFolder);
             }
+
             // Documents
-            MoveFilesOfType(userDefinedPath, ".txt", $"{userDefinedPath}/Documents");
-            MoveFilesOfType(userDefinedPath, ".pdf", $"{userDefinedPath}/Documents");
-            MoveFilesOfType(userDefinedPath, ".docx", $"{userDefinedPath}/Documents");
-            MoveFilesOfType(userDefinedPath, ".psd", $"{userDefinedPath}/Documents");
-            MoveFilesOfType(userDefinedPath, ".kra", $"{userDefinedPath}/Documents");
-            MoveFilesOfType(userDefinedPath, ".aep", $"{userDefinedPath}/Documents");
-            MoveFilesOfType(userDefinedPath, ".csp", $"{userDefinedPath}/Documents");
+            for (int i = 0; i < fileFormatsDocuments.Length; i++)
+            {
+                totalFilesMoved += MoveFilesOfType(userDefinedPath, fileFormatsDocuments[i], $"{userDefinedPath}/Documents");
+            }
+
             // Images
-            MoveFilesOfType(userDefinedPath, ".png", $"{userDefinedPath}/Images");
-            MoveFilesOfType(userDefinedPath, ".jpg", $"{userDefinedPath}/Images");
-            MoveFilesOfType(userDefinedPath, ".jpeg", $"{userDefinedPath}/Images");
-            MoveFilesOfType(userDefinedPath, ".webp", $"{userDefinedPath}/Images");
-            MoveFilesOfType(userDefinedPath, ".gif", $"{userDefinedPath}/Images");
-            MoveFilesOfType(userDefinedPath, ".svg", $"{userDefinedPath}/Images");
+            for (int i = 0; i < fileFormatsImages.Length; i++)
+            {
+                totalFilesMoved += MoveFilesOfType(userDefinedPath, fileFormatsImages[i], $"{userDefinedPath}/Images");
+            }
+
             // Music
-            MoveFilesOfType(userDefinedPath, ".mp3", $"{userDefinedPath}/Music");
-            MoveFilesOfType(userDefinedPath, ".wav", $"{userDefinedPath}/Music");
-            MoveFilesOfType(userDefinedPath, ".aac", $"{userDefinedPath}/Music");
+            for (int i = 0; i < fileFormatsMusic.Length; i++)
+            {
+                totalFilesMoved += MoveFilesOfType(userDefinedPath, fileFormatsMusic[i], $"{userDefinedPath}/Music");
+            }
+
             // Videos
-            MoveFilesOfType(userDefinedPath, ".mp4", $"{userDefinedPath}/Videos");
-            MoveFilesOfType(userDefinedPath, ".mov", $"{userDefinedPath}/Videos");
-            MoveFilesOfType(userDefinedPath, ".mkv", $"{userDefinedPath}/Videos");
-            MoveFilesOfType(userDefinedPath, ".avi", $"{userDefinedPath}/Videos");
-            MoveFilesOfType(userDefinedPath, ".webm", $"{userDefinedPath}/Videos");
+            for (int i = 0; i < fileFormatsVideos.Length; i++)
+            {
+                totalFilesMoved += MoveFilesOfType(userDefinedPath, fileFormatsVideos[i], $"{userDefinedPath}/Videos");
+            }
+
             // Compressed
-            MoveFilesOfType(userDefinedPath, ".zip", $"{userDefinedPath}/Compressed");
-            MoveFilesOfType(userDefinedPath, ".rar", $"{userDefinedPath}/Compressed");
-            MoveFilesOfType(userDefinedPath, ".7z", $"{userDefinedPath}/Compressed");
-            MoveFilesOfType(userDefinedPath, ".tar", $"{userDefinedPath}/Compressed");
-            MoveFilesOfType(userDefinedPath, ".tar.gz", $"{userDefinedPath}/Compressed");
+            for (int i = 0; i < fileFormatsCompressed.Length; i++)
+            {
+                totalFilesMoved += MoveFilesOfType(userDefinedPath, fileFormatsCompressed[i], $"{userDefinedPath}/Compressed");
+            }
+
+            Console.WriteLine($"Total files moved: {totalFilesMoved}");
         }
-        public static void MoveFilesOfType(string sourcePath, string extension, string targetFolder)
+        public static int MoveFilesOfType(string sourcePath, string extension, string targetFolder)
         {
             var files = Directory.EnumerateFiles(sourcePath, $"*{extension}");
+            int countFiles = 0;
             foreach (string file in files)
             {
                 string fileName = file.Substring(sourcePath.Length + 1);
+                Console.WriteLine($"Moving {fileName} to {targetFolder}...");
                 File.Move(file, Path.Combine(targetFolder, fileName));
+                countFiles++;
             }
+            return countFiles;
         }
     }
 }
